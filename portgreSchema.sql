609beee74475af7bf922c4a93d48c53234907cb4
Derek Parson <parson.derek@gmail.com>
2020-01-13 17:00:33 -0800

    Create new table for historical stock prices

--- portgreSchema.sql
+++ portgreSchema.sql
@@ -17,7 +17,15 @@ CREATE TABLE userPortfolio(
   price_when_added int NOT NULL,
   date_when_added date NOT NULL,
   FOREIGN KEY (id) REFERENCES userInfo (id)
-)
+);
+
+CREATE TABLE historicalstockprices(
+  id int NOT NULL PRIMARY KEY,
+  stock_symbol varchar NOT NULL,
+  historical_price double pricision NOT NULL,
+  date_of_price date,
+  FOREIGN KEY (id) REFERENCES userInfo (id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
