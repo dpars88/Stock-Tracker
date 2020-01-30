7c0f91c4d002ca867f504a8359c67c9f29b067eb
Derek Parson <parson.derek@gmail.com>
2020-01-30 15:36:32 -0800

    New idea for problem, on line 27 in StockList.jsx

--- react-client/src/components/StockList.jsx
+++ react-client/src/components/StockList.jsx
@@ -24,6 +24,8 @@ class StockList extends React.Component {
     // })
     // console.log('this should be an array of prices', prices);
 
+    // new idea to use API call that is used for searching since it brings back updated price data use that to get todays price and then use data that was retrieved for each stock in portfolio for graph only
+
     return this.props.items.map((item, index) => {
       const { stock_symbol, price_added } = item;
       let dateAdd = item.date_added;
