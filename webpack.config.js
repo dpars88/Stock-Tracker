e044aedd90a769881dca297c7efcc7c740db0d45
Derek Parson <parson.derek@gmail.com>
2020-04-01 10:52:33 -0700

    Still working

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,6 @@ module.exports = {
 // Still need to fix webpack for CSS Modules to work properly, need to finish reading docs, looking like I will start from scratch to ensure proper setup
 
 
-
 // var path = require('path');
 
 // module.exports = {
