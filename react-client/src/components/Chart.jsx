f82c97d09407f537963c5c5905b4fe1ca7b38195
Derek Parson <parson.derek@gmail.com>
2020-07-24 20:58:55 -0700

    Add spacing to graph, still working on how to get labels to appear as hover over on graph

--- react-client/src/components/Chart.jsx
+++ react-client/src/components/Chart.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { VictoryLine, VictoryChart, VictoryVoronoiContainer, VictoryTheme } from 'victory';
+import { VictoryLine, VictoryChart, VictoryVoronoiContainer, VictoryTheme, VictoryAxis } from 'victory';
 
 //https://formidable.com/open-source/victory/docs/
 
 class Chart extends React.Component {
   constructor(props) {
     super(props);
-  }
-  renderLine() {
-
   }
 
   render() {
@@ -21,10 +18,11 @@ class Chart extends React.Component {
     //var myObj;
     var chartData = [];
     var first = [];
+    var second = [];
 
     if (rawData.length > 0) {
       rawData.map((item) => {
-        //console.log('this is item',item)
+        console.log('this is item',item)
         for(var key in item) {
           var properDate = new Date(key);
           //console.log(properDate)
@@ -45,44 +43,46 @@ class Chart extends React.Component {
           chartData.push(eachArr);
       })
       first = chartData[0]
+      second = chartData[1]
     }
 
     return (
 
       <VictoryChart
-        scale={{x: "time"}}
-        scale={{y: 'linear'}}
-        theme={VictoryTheme.material}
-        // minDomain={{x:0, y:0}}
+        scale={{x: "time", y: "linear"}}
         data={first}
-        // containerComponent={
-        //   <VictoryVoronoiContainer
-        //     labels={({ first }) => `${first['x']}, ${first['y']}`}
-        //   />
-        // }
+        domainPadding={20}
+        padding={75}
+        containerComponent={
+          <VictoryVoronoiContainer
+            // labels={(first) => `Price: + ${first.y}, Date: + ${first.x}`} --- this doesn't make labels like I'd like them to be
+          />
+        }
+        // theme={VictoryTheme.material}
       >
+        <VictoryAxis
+          fixLabelOverlap={true}
+          style={{ tickLabels: { padding: 16, fontSize: 8 } }}
+        />
+        <VictoryAxis
+          dependentAxis={true}
+          fixLabelOverlap={true}
+
+         />
         <VictoryLine
-          interpolation='linear'
+          // labels={(first) => `Price: + ${first.y}, Date: + ${first.x}`} ---- this makes labels everywhere
+          interpolation="natural"
           data={first}
           x={first['x']} y={first['y']}
           style={{
             data: { stroke: "#c43a31" },
             parent: { border: "1px solid #ccc"}
           }}
-          />
-          {/* <VictoryLine
-          interpolation='linear'
-          data={chartData[1]}
-          x={chartData[1]['x']} y={chartData[1]['y']}
-          style={{
-            data: { stroke: "#c43a31" },
-            parent: { border: "1px solid #ccc"}
+          animate={{
+            delay: 2000,
+            duration: 5000,
           }}
-
-          So still working on how to add it multiple lines and have as many lines per the count of items in the portfolio
-
-          /> */}
-
+        />
       </VictoryChart>
 
     );
